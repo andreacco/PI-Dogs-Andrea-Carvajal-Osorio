5879cfa3e84bd34641fee814e110eb0712780d7a
Andreaccodev <andreacc9199@gmail.com>
2025-06-12 03:38:58 -0400

    redeployando el back

--- client/src/redux/Actions/index.js
+++ client/src/redux/Actions/index.js
@@ -11,7 +11,7 @@ export const SORT_BY = 'SORT_BY'
 
 export const getAllDogs = () => async dispatch => {
     try {
-        await axios.get('https://wikidogs-back.onrender.com//dogs')
+        await axios.get('https://wikidogs-back.onrender.com/dogs')
         // await axios.get('http://localhost:3001/dogs')
         // await axios.get('https://pi-dogs-andreacco.herokuapp.com/dogs')
         .then(response => response.data)
@@ -26,7 +26,7 @@ export const getAllDogs = () => async dispatch => {
 export function createDog(newDog){
     return async function (dispatch){
         try {
-            let res = await axios.post('https://wikidogs-back.onrender.com//dogs', newDog)
+            let res = await axios.post('https://wikidogs-back.onrender.com/dogs', newDog)
             // let res = await axios.post('http://localhost:3001/dogs', newDog)
             // let res = await axios.post('https://pi-dogs-andreacco.herokuapp.com/dogs', newDog)
             return dispatch({type: CREATE_DOG, payload: res.data})
@@ -39,7 +39,7 @@ export function createDog(newDog){
 export function getDogDetail(id){
     return async function (dispatch){
         try {
-            let res = await axios.get(`https://wikidogs-back.onrender.com//dogs/${id}`)
+            let res = await axios.get(`https://wikidogs-back.onrender.com/dogs/${id}`)
             // let res = await axios.get(`http://localhost:3001/dogs/${id}`)
             // let res = await axios.get(`https://pi-dogs-andreacco.herokuapp.com/dogs/${id}`)
             return dispatch({type: GET_DOG_DETAIL, payload: res.data})
@@ -52,7 +52,7 @@ export function getDogDetail(id){
 export function searchDog(name){
     return async function (dispatch){
         try {
-            let dog = await axios.get(`https://wikidogs-back.onrender.com//dogs?name=${name}`)
+            let dog = await axios.get(`https://wikidogs-back.onrender.com/dogs?name=${name}`)
             // let dog = await axios.get(`http://localhost:3001/dogs?name=${name}`)
             // let dog = await axios.get(`https://pi-dogs-andreacco.herokuapp.com/dogs?name=${name}`)
             return dispatch({type: SEARCH_DOG, payload: dog.data})
@@ -63,7 +63,7 @@ export function searchDog(name){
 }
 
 export const getTemperaments = () => async dispatch => {
-    return await axios.get("https://wikidogs-back.onrender.com//temperaments")
+    return await axios.get("https://wikidogs-back.onrender.com/temperaments")
     // return await axios.get("http://localhost:3001/temperaments")
     // return await axios.get("https://pi-dogs-andreacco.herokuapp.com/temperaments")
     .then(response => response.data)
